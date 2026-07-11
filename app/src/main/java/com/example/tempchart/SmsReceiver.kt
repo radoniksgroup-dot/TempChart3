7f34a1906d0e1aa6111acb4e5af7955c10013834
radoniksgroup-dot <radoniksgroup@gmail.com>
2026-07-11 10:57:21 +0330

    Update SmsReceiver.kt

--- app/src/main/java/com/example/tempchart/SmsReceiver.kt
+++ app/src/main/java/com/example/tempchart/SmsReceiver.kt
@@ -1,1 +1,23 @@
+package com.example.tempchart
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Telephony
+
+class SmsReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
+            val messages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+            val body = StringBuilder()
+            for (msg in messages) {
+                body.append(msg.messageBody)
+            }
+            val localIntent = Intent("TEMP_SMS_RECEIVED")
+            localIntent.putExtra("body", body.toString())
+            localIntent.setPackage(context.packageName)
+            context.sendBroadcast(localIntent)
+        }
+    }
+}
 
